d83fad830ca03c6a0ceff102304e584fd17fdb7d
bryanngzh <bryanngzihao@gmail.com>
2023-02-20 00:59:45 +0800

    Solve problem2

--- src/problem2/frontend/src/App.js
+++ src/problem2/frontend/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import { Heading, Stack } from '@chakra-ui/react'
+import { Heading, Stack, HStack, Image } from '@chakra-ui/react'
 import Forms from './components/Forms.js';
+import logo from './images/Cz7BPXlw_400x400.png'
 
 function App() {
   return (
     <div className="App">
       <>
+      <HStack spacing={10}>
         <Stack>
           <Heading>
             Transfer $SWTH Tokens
           </Heading>
           <Forms />
         </Stack>
+        <Image src={logo}/>
+      </HStack>
       </>
     </div>
   );
